a42dbe5315a9738a558ee33199cd878723aed191
Isuru Adithya <isuruadithyasandaruwan@gmail.com>
2025-12-16 22:26:22 +0530

    feat: update LandingPage background styling for improved aesthetics

--- web/src/pages/LandingPage.tsx
+++ web/src/pages/LandingPage.tsx
@@ -7,7 +7,7 @@ import InstallPWA from '@/components/InstallPWA';
 
 const LandingPage: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 to-indigo-100 dark:from-gray-900 dark:to-gray-800">
+    <div className="min-h-screen bg-background">
       <InstallPWA />
       <div className="absolute top-4 right-4">
         <ThemeToggle />
